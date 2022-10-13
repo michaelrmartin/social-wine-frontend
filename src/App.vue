cd752d63ebcacbde37031e50b798f30ed07e8c46
michaelrmartin <iammichaelmartin@gmail.com>
2022-10-12 20:12:27 -0500

    update before netlify deploy

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,9 @@ nav a {
 body {
   font-weight: strong;
   font-family: "Raleway", Helvetica, Arial, sans-serif;
-  background-image: url("https://images.unsplash.com/photo-1615799998603-7c6270a45196?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2804&q=80");
+  background-image: url("https://images.pexels.com/photos/6331082/pexels-photo-6331082.jpeg");
   background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
